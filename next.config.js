a335fc399405e498d7fac857d27dd1b649732779
will <tradecall.japan@gmail.com>
2024-12-22 18:45:44 +0800

    fix: ビルドエラーの修正

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
-  },
+    serverActions: {
+      allowedOrigins: ['localhost:3000']
+    }
+  }
 }
 
 module.exports = nextConfig 
